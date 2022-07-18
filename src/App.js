5c5e67989695796540eafd92983398e6ebe95114
firefirehot <rwyat002@ucr.edu>
2022-07-17 21:10:49 -0700

    prepaired images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 
+
 function App() {
   return (
     <div >
       <h1>Website Ready</h1>
+      <img src="images/profile_pic.jpg" alt="profile"/>
+      <img src="images/robert_wyatt.jpg" width="600px" alt="resume pdf"/>
+      <p>Things I will write about.</p>
     </div>
   );
 }
